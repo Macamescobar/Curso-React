dc30611dc7171d35f51e51a58ac8b436d4f22f5a
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2022-10-06 18:43:00 -0300

    add base-pruebas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-DOM/client';
-import { FirstApp } from './FirstApp';
-import {HelloWorldApp} from './HelloWorldApp'
+import { CounterApp } from './CounterApp';
+// import { FirstApp } from './FirstApp';
+// import {HelloWorldApp} from './HelloWorldApp'
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp  />
+        < CounterApp value={ 20 } />
     </React.StrictMode>
 );
