cb80fbbf1f02ce0a1297d233b74ded15c50663b4
Macamescobar <108288476+Macamescobar@users.noreply.github.com>
2022-10-09 19:54:16 -0300

    learning test with jest

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,7 +1,15 @@
+describe (' Pruebas en <DemoComponent>', () => {
+    test('Esta prueba no debe de fallar', () => {
+        // 1. Inicialización
+    
+        const message1 = 'Hola mundo';
+    
+        // 2. Estimulo, lo que se aplica al sujeto de prubea
+    
+        const message2 = message1.trim();
+    
+        // 3. Observar el comportamiento... esperado
+        expect(message1).toBe(message2);
+    });
+});
 
-
-test('Esta prueba no debe de fallar', () => {
-    if (0 === 1) {
-    throw new Error('No puede dividir entre cero');
-    }
-})
